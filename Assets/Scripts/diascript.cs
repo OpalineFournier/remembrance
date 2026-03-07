0dcb08ab5204732623a83079582aa75f3c6ba7ef
Squishy Tumor <totoburke@gmail.com>
2026-03-07 14:43:04 -0500

    I DID THE TUTORIAL AND MADE THE NPCs FACE YOU AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- Assets/Scripts/diascript.cs
+++ Assets/Scripts/diascript.cs
@@ -20,7 +20,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
- 
+  
 
         textComponent.text = string.Empty;
         startDialogue();
