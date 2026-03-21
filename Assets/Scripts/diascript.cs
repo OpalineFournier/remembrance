25c28441b7ba2176137db73d8f621ee592a7fd98
Opaline <fournier.opaline@gmail.com>
2026-03-21 10:40:20 -0400

    Revert "Merge branch 'main' into MannysBranch"
    This reverts commit 1e5bc89ad7dd7a7ea76ca7a0a76178e18375c0f7, reversing
    changes made to a384fc2b5201c34e98d4a60c114c2dbf63f45a7c.

--- Assets/Scripts/diascript.cs
+++ Assets/Scripts/diascript.cs
@@ -21,7 +21,7 @@ public class diascript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-  
+ 
 
         textComponent.text = string.Empty;
         startDialogue();
