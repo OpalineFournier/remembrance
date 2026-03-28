2a884bf9323351c236ef068d9a5434342b655ae4
UltraZavk2 <ultrazavk2@gmail.com>
2026-03-28 13:17:15 -0400

    Added variation to VisionBlockSquare
    Odd and slightly annoying to use, need to revamp further when I have time

--- Assets/Scripts/cutscene1script.cs
+++ Assets/Scripts/cutscene1script.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class cutscene1script : MonoBehaviour
 {
     public float timer;
+
+    public int[] indexvalfadeblack;
+    public int[] indexvalfadewhite;
+    public int[] indexvalfadeout;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,19 +22,25 @@ public class cutscene1script : MonoBehaviour
         timer += Time.fixedDeltaTime;
         if(this.gameObject.tag == "ViewBlocker")
         {
-            if(GameObject.Find("DIalouge Box").GetComponent<diascript>().index == 1 && timer > .02f){
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r + .05f, gameObject.GetComponent<SpriteRenderer>().color.g + .05f, gameObject.GetComponent<SpriteRenderer>().color.b + .05f, 1);
-                timer = 0;
+            foreach(int i in indexvalfadewhite){
+                if(GameObject.Find("DIalouge Box").GetComponent<diascript>().index == i && timer > .02f){
+                    this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r + .05f, gameObject.GetComponent<SpriteRenderer>().color.g + .05f, gameObject.GetComponent<SpriteRenderer>().color.b + .05f, 1);
+                    timer = 0;
+                }
             }
-            else if(GameObject.Find("DIalouge Box").GetComponent<diascript>().index > 1 && GameObject.Find("DIalouge Box").GetComponent<diascript>().index < 9 && timer > .02f)
-            {
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r - .05f, gameObject.GetComponent<SpriteRenderer>().color.g - .05f, gameObject.GetComponent<SpriteRenderer>().color.b - .05f, 1);
-                timer = 0;
+            foreach(int i in indexvalfadeblack){
+                if(GameObject.Find("DIalouge Box").GetComponent<diascript>().index == i && timer > .02f)
+                {
+                    this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r - .05f, gameObject.GetComponent<SpriteRenderer>().color.g - .05f, gameObject.GetComponent<SpriteRenderer>().color.b - .05f, 1);
+                    timer = 0;
+                }
             }
-            else if(GameObject.Find("DIalouge Box").GetComponent<diascript>().index > 9 && timer > .02f)
-            {
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r - .05f, gameObject.GetComponent<SpriteRenderer>().color.g - .05f, gameObject.GetComponent<SpriteRenderer>().color.b - .05f, gameObject.GetComponent<SpriteRenderer>().color.a - .05f);
-                timer = 0;
+            foreach(int i in indexvalfadeout){
+                if(GameObject.Find("DIalouge Box").GetComponent<diascript>().index == i && timer > .02f)
+                {
+                    this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r - .05f, gameObject.GetComponent<SpriteRenderer>().color.g - .05f, gameObject.GetComponent<SpriteRenderer>().color.b - .05f, gameObject.GetComponent<SpriteRenderer>().color.a - .05f);
+                    timer = 0;
+                }
             }
         }
     }
