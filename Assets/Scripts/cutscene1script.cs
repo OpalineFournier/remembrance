c43afacce4e9ab81bb7431b2dab4cd5f8359b338
siaeditionMely <yosmelyvasquez.sia@gmail.com>
2026-04-25 15:45:59 -0400

    first cutscne change

--- Assets/Scripts/cutscene1script.cs
+++ Assets/Scripts/cutscene1script.cs
@@ -21,12 +21,12 @@ public class cutscene1script : MonoBehaviour
                 this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r + .05f, gameObject.GetComponent<SpriteRenderer>().color.g + .05f, gameObject.GetComponent<SpriteRenderer>().color.b + .05f, 1);
                 timer = 0;
             }
-            else if(GameObject.Find("DIalouge Box").GetComponent<diascript>().index > 1 && GameObject.Find("DIalouge Box").GetComponent<diascript>().index < 9 && timer > .02f)
+            else if(GameObject.Find("DIalouge Box").GetComponent<diascript>().index > 1 && GameObject.Find("DIalouge Box").GetComponent<diascript>().index < 7 && timer > .02f)
             {
                 this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r - .05f, gameObject.GetComponent<SpriteRenderer>().color.g - .05f, gameObject.GetComponent<SpriteRenderer>().color.b - .05f, 1);
                 timer = 0;
             }
-            else if(GameObject.Find("DIalouge Box").GetComponent<diascript>().index > 9 && timer > .02f)
+            else if(GameObject.Find("DIalouge Box").GetComponent<diascript>().index > 7 && timer > .02f)
             {
                 this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r - .05f, gameObject.GetComponent<SpriteRenderer>().color.g - .05f, gameObject.GetComponent<SpriteRenderer>().color.b - .05f, gameObject.GetComponent<SpriteRenderer>().color.a - .05f);
                 timer = 0;
