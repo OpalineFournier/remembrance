7482f838b21c8e27fac1b9123fe0d7919083e711
UltraZavk2 <ultrazavk2@gmail.com>
2026-05-09 11:53:11 -0400

    Fixed the rest of the scenes
    Fadeout added to other cutscenes

--- Assets/Scripts/cutscene1script.cs
+++ Assets/Scripts/cutscene1script.cs
@@ -9,6 +9,7 @@ public class cutscene1script : MonoBehaviour
     public int[] indexvalfadeblack;
     public int[] indexvalfadewhite;
     public int[] indexvalfadeout;
+    public int[] indexvalfadein;
 
     // Start is called before the first frame update
     void Start()
@@ -42,7 +43,15 @@ public class cutscene1script : MonoBehaviour
             {
                 if (GameObject.Find("DIalouge Box").GetComponent<diascript>().index == i && timer > .02f)
                 {
-                    this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r - .05f, gameObject.GetComponent<SpriteRenderer>().color.g - .05f, gameObject.GetComponent<SpriteRenderer>().color.b - .05f, gameObject.GetComponent<SpriteRenderer>().color.a - .05f);
+                    this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r, gameObject.GetComponent<SpriteRenderer>().color.g, gameObject.GetComponent<SpriteRenderer>().color.b, gameObject.GetComponent<SpriteRenderer>().color.a - .05f);
+                    timer = 0;
+                }
+            }
+            foreach (int i in indexvalfadein)
+            {
+                if (GameObject.Find("DIalouge Box").GetComponent<diascript>().index == i && timer > .02f)
+                {
+                    this.gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r, gameObject.GetComponent<SpriteRenderer>().color.g, gameObject.GetComponent<SpriteRenderer>().color.b, gameObject.GetComponent<SpriteRenderer>().color.a + .05f);
                     timer = 0;
                 }
             }
